d3e33a348b18c532d2d0f487c62895dc3181efb0
Dvale <vale.daniel@outlook.com>
2025-10-11 17:51:50 +0200

    Add prefix API

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,9 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response()->json([
-        'message' => "Bienvenue sur l'API Parkly",
-        'status' => 'success'
-    ]);
+
+Route::prefix('api')->group(function () {
+    //Initialisation du prefix API mettre toutes les routes dedans
+
+    Route::get('/', function () {
+        return response()->json([
+            'message' => "Bienvenue sur l'API Parkly",
+            'status' => 'success'
+        ]);
+    });
+
+    //Fin du prefix API mettre toutes les route avant
 });
